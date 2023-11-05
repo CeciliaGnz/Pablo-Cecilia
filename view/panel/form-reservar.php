7075579773714b568730d3980d66767afe4a961f
Cecilia González <102307413+CeciliaGnz@users.noreply.github.com>
2023-11-05 15:40:37 -0500

    Update form-reservar.php
    cambiando title

--- view/panel/form-reservar.php
+++ view/panel/form-reservar.php
@@ -8,7 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Perfil de usuario</title>
+        <title>Rerservar equipo</title>
         <link href="../../public/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
         <link href="../../public/css/style.css" rel="stylesheet" />
         <!-- Scrip de iconos local-->
